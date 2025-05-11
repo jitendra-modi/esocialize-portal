d4f6b41d5c3bc3868ab27668fe968db7f6d71f27
jitendra-modi <your-email@example.com>
2025-05-11 13:20:25 +0530

    Update Firebase config to use environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA9TUQRItFJGdgJQlvkeNzsF1EoxGUW34s",
-  authDomain: "e-socialize-management.firebaseapp.com",
-  projectId: "e-socialize-management",
-  storageBucket: "e-socialize-management.firebasestorage.app",
-  messagingSenderId: "1047987512112",
-  appId: "1:1047987512112:web:ef7dbfceb0944d70061ef5",
-  measurementId: "G-ZE1PT2K9TT"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
